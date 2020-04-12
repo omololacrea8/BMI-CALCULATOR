ac1eacf25a5df8f4bd6b8dd308646eadbd32e78c
Fele Omolola <lummy1997@gmail.com>
2020-04-12 18:17:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+//made use of function bmi() because i already use the ONCLICK in my html file
+function BMI(){
+    //used .value because value can be passed to a function and the function will return a value.
+    let height = document.getElementById('height').value; //this method returns the elements that has given ID which is passed to the function
+    let weight = document.getElementById('weight').value;
+    let bmi = weight/(height/100*height/100);
+    let bmical = (bmi.toFixed(2)); //used toFixed() method because it converts a number into string, keeping a specified number of decimals.
+
+    document.getElementById("calcul").innerHTML = "Your BMI is " + bmical;
+
+}
