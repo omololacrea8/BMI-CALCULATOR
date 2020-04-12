a67cb60677c9dd0fbd4e65886ba43832f35be210
Fele Omolola <lummy1997@gmail.com>
2020-04-12 20:12:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 
+
 //made use of function bmi() because i already use the ONCLICK in my html file
 function BMI(){
     //used .value because value can be passed to a function and the function will return a value.
     let height = document.getElementById('height').value; //this method returns the elements that has given ID which is passed to the function
     let weight = document.getElementById('weight').value;
-    let bmi = weight/(height/100*height/100);
-    let bmical = (bmi.toFixed(2)); //used toFixed() method because it converts a number into string, keeping a specified number of decimals.
+    
+        
+    if ((height === '') || (height <= 0) || (isNaN(height)) ){
+        document.getElementById('calcul').innerHTML = "Enter a valid height";
+    }
+    else if (weight === '' || weight < 0 || isNaN(weight)){
+        document.getElementById('calcul').innerHTML = "Enter a valid weight";
 
-    document.getElementById("calcul").innerHTML = "Your BMI is " + bmical;
+    }
+    else{
+        //BMI result
+        let bmi = weight/(height/100*height/100);
+        let bmical = (bmi.toFixed(2)); //used toFixed() method because it converts a number into string, keeping a specified number of decimals.
+       //results
+        document.getElementById("calcul").innerHTML = "Your BMI is " + bmical;
+    }  
 
 }
